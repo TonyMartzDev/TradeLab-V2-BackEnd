6c0627e0957b69f08d86e6dc2e5415446524ee89
Tony.Martz <tony.martz.developer@gmail.com>
2025-04-29 23:45:36 -0700

    created initial database set up file

--- src/db/database.js
+++ src/db/database.js
@@ -1,1 +1,13 @@
-import { sqlite3 } from "sqlite3";
+const sqlite3 = require("sqlite3").verbose();
+
+const DATABASE_PATH = "./trading_app.db";
+
+const db = new sqlite3.Database(DATABASE_PATH, (err) => {
+  if (err) {
+    console.error("Error connecting to database:", err.message);
+  } else {
+    console.log("Connected to database");
+  }
+});
+
+module.exports = db;
